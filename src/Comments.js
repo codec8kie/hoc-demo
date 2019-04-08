41b72be824e6213882c4f9343777558e0166b8d9
ron <business.mingyukim@gmail.com>
2019-04-08 16:24:26 +0900

    map and css fix

--- src/Comments.js
+++ src/Comments.js
@@ -5,9 +5,22 @@ class Comments extends Component {
   render() {
     const { data } = this.props;
 
-    // if (!data) return null;
+    if (!data) return null;
 
-    return <div>{JSON.stringify(data)}</div>;
+    return (
+      <div>
+        {data.map((item, index) => {
+          return (
+            <ul key={index}>
+              <li>{item.id}</li>
+              <li>{item.name}</li>
+              <li>{item.email}</li>
+              <li>{item.body}</li>
+            </ul>
+          );
+        })}
+      </div>
+    );
   }
 }
 
--- src/Post.js
+++ src/Post.js
@@ -5,9 +5,14 @@ class Post extends Component {
   render() {
     const { data } = this.props;
 
-    // if (!data) return null;
+    if (!data) return null;
 
-    return <div>{JSON.stringify(data)}</div>;
+    return (
+      <div>
+        <h3>{data.title}</h3>
+        <p>{data.body}</p>
+      </div>
+    );
   }
 }
 
