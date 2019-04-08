7820c1f4f1f3b2a24e2d0d858ebec164a3e0b04b
ron <business.mingyukim@gmail.com>
2019-04-08 16:11:11 +0900

    first commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,17 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component, lazy, Suspense } from 'react';
+
+const Post = lazy(() => import('./Post'));
+const Comments = lazy(() => import('./Comments'));
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
+      <Suspense fallback={<div>loading...</div>}>
+        <h1>포스트</h1>
+        <Post />
+        <h2>덧글</h2>
+        <Comments />
+      </Suspense>
     );
   }
 }
